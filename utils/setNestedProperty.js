4511d353222649d83141d236b6594bcdf0ded235
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2023-01-05 19:51:26 -0300

    Added set nested props utils

---
+++ utils/setNestedProperty.js
@@ -0,0 +1,9 @@
+export function setNestedProperty(obj, path, value) {
+  const pathArray = Array.isArray(path) ? path : path.match(/([^[.\]])+/g);
+
+  pathArray.reduce((acc, key, i) => {
+    if (acc[key] === undefined) acc[key] = {}
+    if (i === pathArray.length - 1) acc[key] = value
+    return acc[key]
+  }, obj);
+}
